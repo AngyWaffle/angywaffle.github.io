4cc616d97ef2e37ccebf29ecab20d352228620b1
Angy <jacobhellerud@gmail.com>
2023-11-01 10:12:40 +0100

    Test

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function burgerMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+//redirect button to page for oppgave5
+function oppgave5() {
+location.href = "oppgave_5/oppgave_5.html";
+};
+
+function download() {
+    const url = "calcbeta6(werror).py";
+    const a = document.createElement('a')
+    a.href = url
+    a.download = url.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
 }
