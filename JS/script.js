3fcf40c343d7a2a6301ded31d123bbcd6d74fa56
Angy <jacobhellerud@gmail.com>
2023-11-08 14:34:54 +0100

    Update to react

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+import "react.jsx"
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function burgerMenu() {
     var x = document.getElementById("myLinks");
@@ -9,17 +11,18 @@ function burgerMenu() {
 }
 
 
-//redirect button to page for oppgave5
+//redirect button to local page
 function redirect(url) {
 location.href = url;
 };
 
-function angywaffle() {
-    location.href = "https://angywaffle.no";
+//external links need seperate functions
+function angywaffle(url) {
+    location.href = "https://" + url;
 }
 
-function download() {
-    const url = "FirstScript/calcbeta6(werror).py";
+//download function
+function download(url) {
     const a = document.createElement('a')
     a.href = url
     a.download = url.split('/').pop()
