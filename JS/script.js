9f82b2108783e93f93db24702dc01c8fff65d120
Angy <jacobhellerud@gmail.com>
2023-11-13 09:39:12 +0100

    Moving arrays to script

--- JS/script.js
+++ JS/script.js
@@ -42,4 +42,16 @@ function calc6() {
     a.click()
     document.body.removeChild(a)
 }
+
+//Creates the nessecary arrays      
+const texttextarr = ["Kalkulator som tar matte formler. Dette er mitt første script og jeg har jobbet lite på det i etterkant.", "Oppgave 5 er en introduksjon til media querry. ", "Kalkulator current state. Blir oppdatert etter som den blir jobbet med og utviklet. Jobber med GUI.", "AngyWaffle.no er en nettside som jeg bruker for å lære php og databaser."]
+const buttontextarr = ["Last ned .py fil", "Gå til prosjekt","Last ned .py fil", "Gå til prosjekt"]
+const imgsrcarr = ["Images/firstScript.png", "Images/project_img.png", "Images/calc.png", "Images/angywaffle.png"]
+const functionsarr = [calc6, oppg5, calc7, angywaffle] 
+
+let Aktive =[]
+let Innleveringer = []
+let Fritid = []
+let Gamle = []
+
     
